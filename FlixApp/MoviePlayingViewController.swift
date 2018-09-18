18cc31cdda66f23c06885d304d2d3a83254a8455
IsaSam <isaacsamuel27@gmail.com>
2018-09-18 19:13:47 -0400

    connecting data 1

--- FlixApp/MoviePlayingViewController.swift
+++ FlixApp/MoviePlayingViewController.swift
@@ -21,6 +21,9 @@ class MoviePlayingViewController: UIViewController, UITableViewDelegate, UITable
         
         tableView.dataSource = self
         tableView.delegate = self
+        
+        tableView.rowHeight = 168
+        tableView.estimatedRowHeight = 200
 
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
