b5bc53560ab64fccddcb5017870e6bd013e52f6b
IsaSam <isaacsamuel27@gmail.com>
2018-09-19 02:01:31 -0400

    adding alert and customizing cells selection

--- FlixApp/MoviePlayingViewController.swift
+++ FlixApp/MoviePlayingViewController.swift
@@ -21,8 +21,8 @@ class MoviePlayingViewController: UIViewController, UITableViewDelegate, UITable
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: #selector(MoviePlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl.addTarget(self, action: #selector(MoviePlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
 
         tableView.delegate = self
         tableView.rowHeight = 168
@@ -46,14 +46,19 @@ class MoviePlayingViewController: UIViewController, UITableViewDelegate, UITable
         let task = session.dataTask(with: request) {(data, response, error) in
             //-- This will run when the network request returns
             if let error = error{
+                let errorAlertController = UIAlertController(title: "Cannot Get Movies", message: "The Internet connections appears to be offline", preferredStyle: .alert)
+                let cancelAction = UIAlertAction(title: "Retry", style: .cancel)
+                errorAlertController.addAction(cancelAction)
+                self.present(errorAlertController, animated: true)
                 print(error.localizedDescription)
             } else if let data = data,
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]{
-                print(dataDictionary)
+                //print(dataDictionary)
                 self.movies = dataDictionary["results"] as! [[String: Any]]
                 self.tableView.reloadData()
-                self.refreshControl.endRefreshing()
+                
             }
+            self.refreshControl.endRefreshing()
         }
         task.resume()
         activityIndicator.stopAnimating()
@@ -80,7 +85,9 @@ class MoviePlayingViewController: UIViewController, UITableViewDelegate, UITable
 
         return cell
     }
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- README.md
+++ README.md
@@ -15,11 +15,11 @@ The following **required** user stories are complete:
 
 The following **stretch** user stories are implemented:
 
-- [ ] User sees an alert when there's a networking error (+1pt)
+- [X] User sees an alert when there's a networking error (+1pt)
 - [ ] User can search for a movie (+3pt)
 - [X] While poster is being fetched, user see's a placeholder image (+1pt)
 - [ ] User sees image transition for images coming from network, not when it is loaded from cache (+1pt)
-- [ ] Customize the selection effect of the cell (+1pt)
+- [X] Customize the selection effect of the cell (+1pt)
 - [ ] For the large poster, load the low resolution image first and then switch to the high resolution image when complete (+2pt)
 
 The following **additional** user stories are implemented:
