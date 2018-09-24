cc04ba11a64d70a70b2207c8e98cf33a7d92de6a
IsaSam <isaacsamuel27@gmail.com>
2018-09-24 00:26:40 -0400

    Adding the DetailViewController

--- FlixApp/ViewControllers/MoviePlayingViewController.swift
+++ FlixApp/ViewControllers/MoviePlayingViewController.swift
@@ -102,6 +102,14 @@ class MoviePlayingViewController: UIViewController, UITableViewDelegate, UITable
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)
+        let movie = movies[(indexPath?.row)!]
+        let detailViewController = segue.destination as! DetailViewController
+        detailViewController.movie = movie
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
