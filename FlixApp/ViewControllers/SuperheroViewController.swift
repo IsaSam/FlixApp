924a1dbe6bf88a12a0b204e1fdc5b92e57a9d5b9
IsaSam <isaacsamuel27@gmail.com>
2018-09-24 04:10:08 -0400

    Fixed Collection View Layout by code

--- FlixApp/ViewControllers/SuperheroViewController.swift
+++ FlixApp/ViewControllers/SuperheroViewController.swift
@@ -16,8 +16,16 @@ class SuperheroViewController: UIViewController, UICollectionViewDataSource{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         collectionView.dataSource = self
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.minimumInteritemSpacing = 5
+        layout.minimumLineSpacing = layout.minimumInteritemSpacing
+        let cellsPerLine: CGFloat = 2
+        let interItemSpacingTotal = layout.minimumInteritemSpacing * (cellsPerLine - 1)
+        let width = collectionView.frame.size.width / cellsPerLine - interItemSpacingTotal / cellsPerLine
+        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
         fetchMovies()
 
     }
